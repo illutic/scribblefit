9a5da79b44135555b80050139f5b0295f8eb2c4b
george.sigalas <george.sigalas@arrive.com>
2026-06-14 14:22:16 +0100

    build(android): Update the output aab location

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "../../keystore/release.keystore")
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "./keystore/release.keystore")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
